7a6495d99ff361f45bd17593429c6618816e2433
Alec Larson <alec.stanford.larson@gmail.com>
2020-01-02 12:29:26 -0500

    fix(macos): stop using KeyboardAvoidingView

--- app/react-native/src/preview/components/OnDeviceUI/index.tsx
+++ app/react-native/src/preview/components/OnDeviceUI/index.tsx
@@ -1,33 +1,11 @@
 import React, { PureComponent } from 'react';
-import {
-  Keyboard,
-  KeyboardAvoidingView,
-  Platform,
-  TouchableOpacity,
-  TouchableOpacityProps,
-} from 'react-native';
-import { a, SpringValue } from '@react-spring/native';
-import styled from '@emotion/native';
+import { View } from 'react-native';
+import { SpringValue } from '@react-spring/native';
 import addons from '@storybook/addons';
 import Channel from '@storybook/channels';
 import StoryListView from '../StoryListView';
 import StoryView from '../StoryView';
 import Addons from './addons';
-import Panel from './panel';
-import Navigation from './navigation';
-import AbsolutePositionedKeyboardAwareView, {
-  PreviewDimens,
-} from './absolute-positioned-keyboard-aware-view';
-import { PREVIEW } from './navigation/constants';
-import {
-  getPreviewPosition,
-  getPreviewScale,
-  getAddonPanelPosition,
-  getNavigatorPanelPosition,
-} from './animation';
-import { EmotionProps } from '../Shared/theme';
-
-const IS_IOS = Platform.OS === 'ios';
 
 interface OnDeviceUIProps {
   stories: any;
@@ -38,25 +16,6 @@ interface OnDeviceUIProps {
   keyboardAvoidingViewVerticalOffset?: number;
 }
 
-interface OnDeviceUIState {
-  tabOpen: number;
-  slideBetweenAnimation: boolean;
-  previewWidth: number;
-  previewHeight: number;
-}
-
-type EmotionPreviewProps = EmotionProps & TouchableOpacityProps;
-
-const Preview: typeof TouchableOpacity = styled.TouchableOpacity`
-  flex: 1;
-  border-left-width: ${(props: EmotionPreviewProps) => (props.disabled ? '0' : '1')};
-  border-top-width: ${(props: EmotionPreviewProps) => (props.disabled ? '0' : '1')};
-  border-right-width: ${(props: EmotionPreviewProps) => (props.disabled ? '0' : '1')};
-  border-bottom-width: ${(props: EmotionPreviewProps) => (props.disabled ? '0' : '1')};
-  border-color: ${(props: EmotionPreviewProps) =>
-    props.disabled ? 'transparent' : props.theme.previewBorderColor};
-`;
-
 export default class OnDeviceUI extends PureComponent<OnDeviceUIProps, OnDeviceUIState> {
   animatedValue: SpringValue;
 
@@ -64,100 +23,21 @@ export default class OnDeviceUI extends PureComponent<OnDeviceUIProps, OnDeviceU
 
   constructor(props: OnDeviceUIProps) {
     super(props);
-    const tabOpen = props.tabOpen || PREVIEW;
-    this.state = {
-      tabOpen,
-      slideBetweenAnimation: false,
-      previewWidth: 0,
-      previewHeight: 0,
-    };
-    this.animatedValue = new SpringValue('tabOpen').set(tabOpen);
     this.channel = addons.getChannel();
   }
 
-  onLayout = ({ previewWidth, previewHeight }: PreviewDimens) => {
-    this.setState({ previewWidth, previewHeight });
-  };
-
-  handleOpenPreview = () => {
-    this.handleToggleTab(PREVIEW);
-  };
-
-  handleToggleTab = (newTabOpen: number) => {
-    const { tabOpen } = this.state;
-    if (newTabOpen === tabOpen) {
-      return;
-    }
-    this.animatedValue.start({
-      to: newTabOpen,
-    });
-    this.setState({
-      tabOpen: newTabOpen,
-      // True if swiping between navigator and addons
-      slideBetweenAnimation: tabOpen + newTabOpen === PREVIEW,
-    });
-    if (Platform.OS !== 'macos') {
-      // close the keyboard opened from a TextInput from story list or knobs
-      if (newTabOpen === PREVIEW) {
-        Keyboard.dismiss();
-      }
-    }
-  };
-
   render() {
-    const {
-      stories,
-      url,
-      isUIHidden,
-      shouldDisableKeyboardAvoidingView,
-      keyboardAvoidingViewVerticalOffset,
-    } = this.props;
-
-    const { tabOpen, slideBetweenAnimation, previewWidth, previewHeight } = this.state;
-
-    const previewWrapperStyles = [
-      { flex: 1 },
-      getPreviewPosition(this.animatedValue, previewWidth, previewHeight, slideBetweenAnimation),
-    ];
-
-    const previewStyles = [{ flex: 1 }, getPreviewScale(this.animatedValue, slideBetweenAnimation)];
-
+    const { stories, url } = this.props;
     return (
-      <KeyboardAvoidingView
-        enabled={!shouldDisableKeyboardAvoidingView || tabOpen !== PREVIEW}
-        behavior={IS_IOS ? 'padding' : null}
-        keyboardVerticalOffset={keyboardAvoidingViewVerticalOffset}
-        style={{ position: 'absolute', width: '100%', height: '100%' }}
-      >
-        <AbsolutePositionedKeyboardAwareView
-          onLayout={this.onLayout}
-          previewHeight={previewHeight}
-          previewWidth={previewWidth}
-        >
-          <a.View style={previewWrapperStyles}>
-            <a.View style={previewStyles}>
-              <Preview
-                accessible={false}
-                disabled={tabOpen === PREVIEW}
-                onPress={this.handleOpenPreview}
-              >
-                <StoryView url={url} onDevice stories={stories} />
-              </Preview>
-            </a.View>
-          </a.View>
-          <Panel style={getNavigatorPanelPosition(this.animatedValue, previewWidth)}>
-            <StoryListView stories={stories} />
-          </Panel>
-          <Panel style={getAddonPanelPosition(this.animatedValue, previewWidth)}>
-            <Addons />
-          </Panel>
-        </AbsolutePositionedKeyboardAwareView>
-        <Navigation
-          tabOpen={tabOpen}
-          onChangeTab={this.handleToggleTab}
-          initialUiVisible={!isUIHidden}
-        />
-      </KeyboardAvoidingView>
+      <View style={{ position: 'absolute', width: '100%', height: '100%', flexDirection: 'row' }}>
+        <View style={{ width: '20%', height: '100%' }}>
+          <StoryListView stories={stories} />
+        </View>
+        <StoryView url={url} onDevice stories={stories} />
+        <View style={{ width: '20%', height: '100%' }}>
+          <Addons />
+        </View>
+      </View>
     );
   }
 }
