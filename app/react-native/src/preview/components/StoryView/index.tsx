2b9363d93fecff8d595011cecb566a418b44f94d
Alec Larson <alec.stanford.larson@gmail.com>
2020-01-02 12:27:19 -0500

    nit: add horizontal padding to story view

--- app/react-native/src/preview/components/StoryView/index.tsx
+++ app/react-native/src/preview/components/StoryView/index.tsx
@@ -76,7 +76,7 @@ export default class StoryView extends Component<Props> {
     if (story && story.storyFn) {
       const { id, storyFn: Story } = story;
       return (
-        <View key={id} testID={id} style={{ flex: 1 }}>
+        <View key={id} testID={id} style={{ flex: 1, paddingHorizontal: '1%' }}>
           <Story />
         </View>
       );
