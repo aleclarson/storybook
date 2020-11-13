cdfb56769a1e4968e5c0cfe6c6f3d65ebb7e6ae6
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-11-13 10:34:31 -0500

    chore: remove most "app/*" packages

--- app/html/src/server/options.ts
+++
@@ -1,7 +0,0 @@
-// tslint:disable-next-line: no-var-requires
-const packageJson = require('../../package.json');
-
-export default {
-  packageJson,
-  frameworkPresets: [require.resolve('./framework-preset-html.js')],
-};
--- app/preact/src/server/options.ts
+++
@@ -1,6 +0,0 @@
-const packageJson = require('../../package.json');
-
-export default {
-  packageJson,
-  frameworkPresets: [require.resolve('./framework-preset-preact.js')],
-};
