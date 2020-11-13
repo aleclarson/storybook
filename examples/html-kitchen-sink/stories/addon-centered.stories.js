28266ecd2970336b94df14091e567e61e11ceeb3
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-11-13 10:48:27 -0500

    chore: remove examples

--- examples/html-kitchen-sink/stories/addon-centered.stories.js
+++
@@ -1,9 +0,0 @@
-import centered from '@storybook/addon-centered/html';
-
-export default {
-  title: 'Addons|Centered',
-  decorators: [centered],
-};
-
-export const story1 = () => '<button>I am a Button !</button>';
-story1.story = { name: 'button in center' };
