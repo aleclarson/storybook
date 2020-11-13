33a4586cfcd015f8f2d57b7e5f5d911c6c87d140
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-11-13 14:08:03 -0500

    build: clear dist when scripts/prepare fails
    and skip when dist exists

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -1,9 +1,12 @@
 /* eslint-disable no-console */
 /* tslint:disable:no-console */
+const fs = require('fs');
+if (fs.existsSync('dist')) {
+  return;
+}
 const path = require('path');
 const shell = require('shelljs');
 const chalk = require('chalk');
-const fs = require('fs');
 const log = require('npmlog');
 const { babelify } = require('./compile-babel');
 const { tscfy } = require('./compile-tsc');
@@ -13,10 +16,6 @@ function getPackageJson() {
 
   // eslint-disable-next-line global-require,import/no-dynamic-require
   return require(path.join(modulePath, 'package.json'));
-}
-
-function removeDist() {
-  shell.rm('-rf', 'dist');
 }
 
 const ignore = ['__mocks__', '__snapshots__', '__tests__', '/tests/', /.+\.test\..+/];
@@ -40,7 +39,10 @@ function cleanup() {
   }
 }
 
+let failed = false;
+
 function logError(type, packageJson, errorLogs) {
+  failed = true;
   log.error(`FAILED (${type}) : ${errorLogs}`);
   log.error(
     `FAILED to compile ${type}: ${chalk.bold(`${packageJson.name}@${packageJson.version}`)}`
@@ -49,11 +51,13 @@ function logError(type, packageJson, errorLogs) {
 
 const packageJson = getPackageJson();
 
-removeDist();
-
 babelify({ errorCallback: errorLogs => logError('js', packageJson, errorLogs) });
 tscfy({ errorCallback: errorLogs => logError('ts', packageJson, errorLogs) });
 
-cleanup();
+if (failed) {
+  shell.rm('-rf', 'dist');
+  process.exit(1);
+}
 
+cleanup();
 console.log(chalk.gray(`Built: ${chalk.bold(`${packageJson.name}@${packageJson.version}`)}`));
