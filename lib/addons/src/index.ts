e6dfd9b6d666e6d765895b69cd6547b0cca17ac2
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-11-15 12:48:22 -0500

    fix: use `import =` with "global" package

--- lib/addons/src/index.ts
+++ lib/addons/src/index.ts
@@ -1,4 +1,4 @@
-import global from 'global';
+import global = require('global');
 import { ReactElement } from 'react';
 import { Channel } from '@storybook/channels';
 import { API } from '@storybook/api';
